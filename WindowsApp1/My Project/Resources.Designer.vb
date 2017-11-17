74e832b5f7b425d02295246d66a352e2f1b01dea
EkinoTz <EkinoTz@gmx.de>
2017-11-18 00:25:57 +0100

    Added Stuff to Item List

--- WindowsApp1/My Project/Resources.Designer.vb
+++ WindowsApp1/My Project/Resources.Designer.vb
@@ -253,6 +253,16 @@ Namespace My.Resources
         '''<summary>
         '''  Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property GOLD() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("GOLD", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property ID_1() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("ID 1", resourceCulture)
