c49fa2ae802031a994aa6cd6529edc036b12e355
EkinoTz <EkinoTz@gmx.de>
2017-11-27 19:49:15 +0100

    Window Changed

--- WindowsApp1/ItemListHTML.vb
+++ WindowsApp1/ItemListHTML.vb
@@ -2,4 +2,8 @@
     Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
 
     End Sub
+
+    Private Sub MephButton1_Click(sender As Object, e As EventArgs) Handles MephButton1.Click
+        Close()
+    End Sub
 End Class
