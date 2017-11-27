738876f556b8935aa3ab4ee6791f8d95ce9b80c9
EkinoTz <EkinoTz@gmx.de>
2017-11-27 19:04:39 +0100

    WebBrowser Removed

--- WindowsApp1/ItemListHTML.Designer.vb
+++ WindowsApp1/ItemListHTML.Designer.vb
@@ -23,15 +23,12 @@ Partial Class ItemListHTML
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.MephTheme1 = New WindowsApp1.MephTheme()
-        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
-        Me.MephTheme1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MephTheme1
         '
         Me.MephTheme1.AccentColor = System.Drawing.Color.DarkRed
         Me.MephTheme1.BackColor = System.Drawing.Color.FromArgb(CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer))
-        Me.MephTheme1.Controls.Add(Me.WebBrowser1)
         Me.MephTheme1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.MephTheme1.Location = New System.Drawing.Point(0, 0)
         Me.MephTheme1.Name = "MephTheme1"
@@ -39,14 +36,6 @@ Partial Class ItemListHTML
         Me.MephTheme1.SubHeader = ""
         Me.MephTheme1.TabIndex = 0
         Me.MephTheme1.Text = "Item List"
-        '
-        'WebBrowser1
-        '
-        Me.WebBrowser1.Location = New System.Drawing.Point(0, 35)
-        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
-        Me.WebBrowser1.Name = "WebBrowser1"
-        Me.WebBrowser1.Size = New System.Drawing.Size(1106, 571)
-        Me.WebBrowser1.TabIndex = 0
         '
         'ItemListHTML
         '
@@ -58,11 +47,9 @@ Partial Class ItemListHTML
         Me.Name = "ItemListHTML"
         Me.Text = "ItemListHTML"
         Me.TransparencyKey = System.Drawing.Color.Fuchsia
-        Me.MephTheme1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents MephTheme1 As MephTheme
-    Friend WithEvents WebBrowser1 As WebBrowser
 End Class
