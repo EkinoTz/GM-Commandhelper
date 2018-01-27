164e26de68ba469ce9688272becd8b24881d412f
EkinoTz <EkinoTz@gmx.de>
2018-01-27 04:19:37 +0100

    Done.

---
+++ NosLegion Workspace/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ThirteenForm1 = New NosLegion_Workspace.ThirteenForm()
+        Me.ThirteenButton1 = New NosLegion_Workspace.ThirteenButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ThirteenForm1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ThirteenForm1
+        '
+        Me.ThirteenForm1.AccentColor = System.Drawing.Color.DodgerBlue
+        Me.ThirteenForm1.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.ThirteenForm1.ColorScheme = NosLegion_Workspace.ThirteenForm.ColorSchemes.Dark
+        Me.ThirteenForm1.Controls.Add(Me.ThirteenButton1)
+        Me.ThirteenForm1.Controls.Add(Me.Panel1)
+        Me.ThirteenForm1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ThirteenForm1.Font = New System.Drawing.Font("Segoe UI Semilight", 9.75!)
+        Me.ThirteenForm1.ForeColor = System.Drawing.Color.White
+        Me.ThirteenForm1.Location = New System.Drawing.Point(0, 0)
+        Me.ThirteenForm1.Name = "ThirteenForm1"
+        Me.ThirteenForm1.Size = New System.Drawing.Size(699, 704)
+        Me.ThirteenForm1.TabIndex = 0
+        Me.ThirteenForm1.Text = "Workspace NosLegion"
+        '
+        'ThirteenButton1
+        '
+        Me.ThirteenButton1.AccentColor = System.Drawing.Color.DodgerBlue
+        Me.ThirteenButton1.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.ThirteenButton1.ColorScheme = NosLegion_Workspace.ThirteenButton.ColorSchemes.Dark
+        Me.ThirteenButton1.Font = New System.Drawing.Font("Segoe UI Semilight", 9.75!)
+        Me.ThirteenButton1.ForeColor = System.Drawing.Color.White
+        Me.ThirteenButton1.Location = New System.Drawing.Point(674, 3)
+        Me.ThirteenButton1.Name = "ThirteenButton1"
+        Me.ThirteenButton1.Size = New System.Drawing.Size(22, 23)
+        Me.ThirteenButton1.TabIndex = 1
+        Me.ThirteenButton1.Text = "x"
+        Me.ThirteenButton1.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(3, 34)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(693, 667)
+        Me.Panel1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(699, 704)
+        Me.Controls.Add(Me.ThirteenForm1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ThirteenForm1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ThirteenForm1 As ThirteenForm
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ThirteenButton1 As ThirteenButton
+End Class
---
+++ NosLegion Workspace/Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports CefSharp.WinForms
+Imports CefSharp
+
+Public Class Form1
+
+    Private WithEvents browser As ChromiumWebBrowser
+
+    Public Sub New()
+        InitializeComponent()
+
+        Dim settings As New CefSettings()
+        CefSharp.Cef.Initialize(settings)
+
+        browser = New ChromiumWebBrowser("https://docs.google.com/spreadsheets/d/1sChRyXQKywtdlaR-Z9HiU360tN6fEq60Alo31kMkw0s/edit#gid=0") With {
+            .Dock = DockStyle.Fill
+        }
+        Panel1.Controls.Add(browser)
+
+    End Sub
+
+    Private Sub ThirteenButton1_Click(sender As Object, e As EventArgs) Handles ThirteenButton1.Click
+        Close()
+    End Sub
+End Class
