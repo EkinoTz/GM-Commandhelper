d5ae8fede301a80d1bd20f82845a2270df464fa9
Hunter <celestialnos@outlook.de>
2018-01-27 02:01:31 +0100

    Dunno

--- WindowsApp1/CommandVB/GameMaster/Zoom.Designer.vb
+++ WindowsApp1/CommandVB/GameMaster/Zoom.Designer.vb
@@ -23,7 +23,11 @@ Partial Class Zoom
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.MephTheme1 = New WindowsApp1.MephTheme()
+        Me.MephButton1 = New WindowsApp1.MephButton()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.MephTextBox3 = New WindowsApp1.MephTextBox()
+        Me.MephTextBox2 = New WindowsApp1.MephTextBox()
+        Me.MephTextBox1 = New WindowsApp1.MephTextBox()
         Me.MephTheme1.SuspendLayout()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -32,6 +36,9 @@ Partial Class Zoom
         '
         Me.MephTheme1.AccentColor = System.Drawing.Color.DarkRed
         Me.MephTheme1.BackColor = System.Drawing.Color.FromArgb(CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer))
+        Me.MephTheme1.Controls.Add(Me.MephButton1)
+        Me.MephTheme1.Controls.Add(Me.MephTextBox3)
+        Me.MephTheme1.Controls.Add(Me.MephTextBox2)
         Me.MephTheme1.Controls.Add(Me.PictureBox2)
         Me.MephTheme1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.MephTheme1.Location = New System.Drawing.Point(0, 0)
@@ -40,6 +47,16 @@ Partial Class Zoom
         Me.MephTheme1.SubHeader = "Insert Sub Header"
         Me.MephTheme1.TabIndex = 0
         Me.MephTheme1.Text = "MephTheme1"
+        '
+        'MephButton1
+        '
+        Me.MephButton1.BackColor = System.Drawing.Color.Transparent
+        Me.MephButton1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer))
+        Me.MephButton1.Location = New System.Drawing.Point(12, 227)
+        Me.MephButton1.Name = "MephButton1"
+        Me.MephButton1.Size = New System.Drawing.Size(260, 23)
+        Me.MephButton1.TabIndex = 43
+        Me.MephButton1.Text = "Back"
         '
         'PictureBox2
         '
@@ -54,14 +71,60 @@ Partial Class Zoom
         Me.PictureBox2.TabIndex = 40
         Me.PictureBox2.TabStop = False
         '
-        'ChannelInfo
+        'MephTextBox3
+        '
+        Me.MephTextBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.MephTextBox3.ForeColor = System.Drawing.Color.Silver
+        Me.MephTextBox3.Location = New System.Drawing.Point(12, 163)
+        Me.MephTextBox3.MaxLength = 32767
+        Me.MephTextBox3.MultiLine = False
+        Me.MephTextBox3.Name = "MephTextBox3"
+        Me.MephTextBox3.Size = New System.Drawing.Size(260, 24)
+        Me.MephTextBox3.TabIndex = 42
+        Me.MephTextBox3.Text = "Will be resetted with each map change"
+        Me.MephTextBox3.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.MephTextBox3.UseSystemPasswordChar = False
+        Me.MephTextBox3.WordWrap = False
+        '
+        'MephTextBox2
+        '
+        Me.MephTextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.MephTextBox2.ForeColor = System.Drawing.Color.Silver
+        Me.MephTextBox2.Location = New System.Drawing.Point(12, 118)
+        Me.MephTextBox2.MaxLength = 32767
+        Me.MephTextBox2.MultiLine = False
+        Me.MephTextBox2.Name = "MephTextBox2"
+        Me.MephTextBox2.Size = New System.Drawing.Size(260, 24)
+        Me.MephTextBox2.TabIndex = 41
+        Me.MephTextBox2.Text = "Max Value is 59"
+        Me.MephTextBox2.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.MephTextBox2.UseSystemPasswordChar = False
+        Me.MephTextBox2.WordWrap = False
+        '
+        'MephTextBox1
+        '
+        Me.MephTextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.MephTextBox1.ForeColor = System.Drawing.Color.Silver
+        Me.MephTextBox1.Location = New System.Drawing.Point(12, 74)
+        Me.MephTextBox1.MaxLength = 32767
+        Me.MephTextBox1.MultiLine = False
+        Me.MephTextBox1.Name = "MephTextBox1"
+        Me.MephTextBox1.Size = New System.Drawing.Size(260, 24)
+        Me.MephTextBox1.TabIndex = 41
+        Me.MephTextBox1.Text = "Makes your Scroll out higher"
+        Me.MephTextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.MephTextBox1.UseSystemPasswordChar = False
+        Me.MephTextBox1.WordWrap = False
+        '
+        'Zoom
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.MephTextBox1)
         Me.Controls.Add(Me.MephTheme1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
-        Me.Name = "Guri"
+        Me.Name = "Zoom"
         Me.Text = "Guri"
         Me.TransparencyKey = System.Drawing.Color.Fuchsia
         Me.MephTheme1.ResumeLayout(False)
@@ -72,4 +135,8 @@ Partial Class Zoom
 
     Friend WithEvents MephTheme1 As MephTheme
     Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents MephButton1 As MephButton
+    Friend WithEvents MephTextBox3 As MephTextBox
+    Friend WithEvents MephTextBox2 As MephTextBox
+    Friend WithEvents MephTextBox1 As MephTextBox
 End Class
--- WindowsApp1/CommandVB/GameMaster/Zoom.vb
+++ WindowsApp1/CommandVB/GameMaster/Zoom.vb
@@ -1,3 +1,32 @@
 ﻿Public Class Zoom
 
+    Private Sub MephButton1_Click(sender As Object, e As EventArgs) Handles MephButton1.Click
+        Me.MephButton1.BackColor = System.Drawing.Color.Transparent
+        Me.MephButton1.TabStop = False
+        Me.MephButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MephButton1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer))
+        Me.MephButton1.Location = New System.Drawing.Point(12, 201)
+        Me.MephButton1.Name = "MephButton1"
+        Me.MephButton1.Size = New System.Drawing.Size(260, 23)
+        Me.MephButton1.TabIndex = 43
+        Me.MephButton1.TabStop = False
+        Me.MephButton1.Text = "Back"
+    End Sub
+
+    Private Sub MephTextBox1_Click(sender As Object, e As EventArgs) Handles MephTextBox1.Click
+        Me.MephTextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.MephTextBox1.CausesValidation = False
+        Me.MephTextBox1.Enabled = False
+        Me.MephTextBox1.ForeColor = System.Drawing.Color.Silver
+        Me.MephTextBox1.Location = New System.Drawing.Point(12, 68)
+        Me.MephTextBox1.MaxLength = 32767
+        Me.MephTextBox1.MultiLine = True
+        Me.MephTextBox1.Name = "MephTextBox1"
+        Me.MephTextBox1.Size = New System.Drawing.Size(260, 38)
+        Me.MephTextBox1.TabIndex = 44
+        Me.MephTextBox1.TabStop = False
+        Me.MephTextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center
+        Me.MephTextBox1.UseSystemPasswordChar = False
+        Me.MephTextBox1.WordWrap = True
+    End Sub
 End Class
